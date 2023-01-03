49c8c1858310143f3795d99f937955e5dcf95316
Akinsanmi Timothy <timwebdev147@gmail.com>
2023-01-03 17:57:07 +0100

    Cors update for deployed Front end

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(cors())
+app.use(cors({
+    origin: 'https://mern-task-app-frontend.vercel.app'
+}))
 app.use('/api/tasks', taskRoutes)
 
 
